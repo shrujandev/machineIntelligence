ae1a9415fa01ffd89bf0df59f0c2d14f689945c7
shrujandev <shrujandev1824@gmail.com>
2022-09-09 15:17:32 +0530

    DFS successfully running (has to reverse the order of traversal)

--- PES1UG20CS415.py
+++ PES1UG20CS415.py
@@ -17,29 +17,29 @@ def A_star_Traversal(cost, heuristic, start_point, goals):
     """
     path = []
     # TODO
-    path = []
-    front = [start_point]
-    frontier = [[0+heuristic[start_point], front]]
-    while len(frontier) > 0:
-        curr_cost, curr_path = frontier.pop(0)
-        n = curr_path[-1]
-        curr_cost -= heuristic[n]
-        if n in goals:
-            return curr_path
-        path.append(n)
-        children = [i for i in range(len(cost[0]))
-                    if cost[n][i] not in [0, -1]]
-        for i in children:
-            new_curr_path = curr_path + [i]
-            new_path_cost = curr_cost + cost[n][i] + heuristic[i]
-            if i not in path and new_curr_path not in [i[1] for i in frontier]:
-                frontier.append((new_path_cost, new_curr_path))
-                frontier = sorted(frontier, key=lambda x: (x[0], x[1]))
-            elif new_curr_path in [i[1] for i in frontier]:
-                index = search_q(frontier, new_curr_path)
-                frontier[index][0] = min(frontier[index][0], new_path_cost)
-                frontier = sorted(frontier, key=lambda x: (x[0], x[1]))
-    return path
+    # path = []
+    # front = [start_point]
+    # frontier = [[0+heuristic[start_point], front]]
+    # while len(frontier) > 0:
+    #     curr_cost, curr_path = frontier.pop(0)
+    #     n = curr_path[-1]
+    #     curr_cost -= heuristic[n]
+    #     if n in goals:
+    #         return curr_path
+    #     path.append(n)
+    #     children = [i for i in range(len(cost[0]))
+    #                 if cost[n][i] not in [0, -1]]
+    #     for i in children:
+    #         new_curr_path = curr_path + [i]
+    #         new_path_cost = curr_cost + cost[n][i] + heuristic[i]
+    #         if i not in path and new_curr_path not in [i[1] for i in frontier]:
+    #             frontier.append((new_path_cost, new_curr_path))
+    #             frontier = sorted(frontier, key=lambda x: (x[0], x[1]))
+    #         elif new_curr_path in [i[1] for i in frontier]:
+    #             index = search_q(frontier, new_curr_path)
+    #             frontier[index][0] = min(frontier[index][0], new_path_cost)
+    #             frontier = sorted(frontier, key=lambda x: (x[0], x[1]))
+    # return path
     return path
 
 
