c05d79e352843a5b77969e9acb52752757d32cb6
shrujandev <shrujandev1824@gmail.com>
2022-09-09 17:27:05 +0530

    A* algorithm working

--- PES1UG20CS415.py
+++ PES1UG20CS415.py
@@ -4,6 +4,9 @@ Do not modify the given functions
 """
 
 
+from operator import ne
+
+
 def A_star_Traversal(cost, heuristic, start_point, goals):
     """
     Perform A* Traversal and find the optimal path 
@@ -16,30 +19,34 @@ def A_star_Traversal(cost, heuristic, start_point, goals):
         path: path to goal state obtained from A*(list of ints)
     """
     path = []
+    frontier = []
+    frontier.append([heuristic[start_point], [start_point]])
+
+    while frontier:
+        curCost, pathTaken = frontier.pop(0)
+        n = pathTaken[-1]
+        curCost -= heuristic[n]
+        if n in goals:
+            return pathTaken
+        path.append(n)
+
+        subTree = []
+        for i in range(len(cost[0])):
+            if cost[n][i] > 0:
+                subTree.append(i)
+
+        for node in subTree:
+            newPath = pathTaken + [node]
+            newCost = curCost + cost[n][node]+heuristic[node]
+            if node not in path and newPath not in [j[1] for j in frontier]:
+                frontier.append((newCost, newPath))
+                frontier.sort(key=lambda x: (x[0], x[1]))
+            elif newPath in [i[1] for i in frontier]:
+                index = frontier.index(newPath)
+                frontier[index][0] = min(frontier[index][0], newCost)
+                frontier.sort(key=lambda x: (x[0], x[1]))
+
     # TODO
-    # path = []
-    # front = [start_point]
-    # frontier = [[0+heuristic[start_point], front]]
-    # while len(frontier) > 0:
-    #     curr_cost, curr_path = frontier.pop(0)
-    #     n = curr_path[-1]
-    #     curr_cost -= heuristic[n]
-    #     if n in goals:
-    #         return curr_path
-    #     path.append(n)
-    #     children = [i for i in range(len(cost[0]))
-    #                 if cost[n][i] not in [0, -1]]
-    #     for i in children:
-    #         new_curr_path = curr_path + [i]
-    #         new_path_cost = curr_cost + cost[n][i] + heuristic[i]
-    #         if i not in path and new_curr_path not in [i[1] for i in frontier]:
-    #             frontier.append((new_path_cost, new_curr_path))
-    #             frontier = sorted(frontier, key=lambda x: (x[0], x[1]))
-    #         elif new_curr_path in [i[1] for i in frontier]:
-    #             index = search_q(frontier, new_curr_path)
-    #             frontier[index][0] = min(frontier[index][0], new_path_cost)
-    #             frontier = sorted(frontier, key=lambda x: (x[0], x[1]))
-    # return path
     return path
 
 
